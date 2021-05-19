bba6fae03e587ae4c64778099d12fdfdf425c301
Ciprian Brebu <brebu.ciprian@gmail.com>
2021-05-19 16:41:33 +0000

    Added vpc module

---
+++ vpc.tf
@@ -0,0 +1,18 @@
+module "network" {
+  source  = "app.terraform.io/xywex/network/google"
+  version = "2.5.0"
+
+
+  project_id   = var.project
+  network_name = "gaurav-network"
+  routing_mode = "GLOBAL"
+
+  subnets = [
+  {
+    subnet_name   = "gaurav-subnet"
+    subnet_ip     = "10.100.10.0/24"
+    subnet_region = var.region
+  }
+]
+
+}
